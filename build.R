0735e24d29a9d2333d65e3635468e9e1470ad994
Michael Sachs <sachsmc@gmail.com>
2023-05-02 12:13:15 +0200

    rebuild homwpage

--- build.R
+++ build.R
@@ -9,11 +9,3 @@ ch <- check_for_cran(".", show_status = FALSE)
 ch$update()
 
 pkgdown::build_site(install = FALSE)
-## fix path to images
-lines <- readLines("docs/articles/shinyapp.html")
-lines <- gsub("../../../../OneDrive%20-%20KI.SE/Code/causaloptim/vignettes/", "", lines)
-writeLines(lines, con = "docs/articles/shinyapp.html")
-
-lines <- readLines("docs/articles/vertexenum-speed.html")
-lines <- gsub("../../../../OneDrive%20-%20KI.SE/Code/causaloptim/vignettes/", "", lines)
-writeLines(lines, con = "docs/articles/vertexenum-speed.html")
